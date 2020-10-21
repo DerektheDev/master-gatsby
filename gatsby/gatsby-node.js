aae76f003136cc00770f9ea82f983d954490340b
Derek Montgomery <derekm@ifit.com>
2020-10-21 14:52:35 -0500

    pagination

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -93,6 +93,44 @@ async function fetchBeersAndTurnIntoNodes({
   });
 }
 
+async function turnSliceMastersIntoPages({ graphql, actions }) {
+  // 1. Query all slicemasters
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  // 2. TODO: Turn each slicemaster into their own page
+  // 3. Figure out how many pages there are based on
+  //    # of slicemasters and how many per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+
+  // 4. Loop from 1 - n and create pages for them
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      // data is passed to the template when created
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
+}
+
 export async function sourceNodes(params) {
   // fetch a list of beers and source them into our gatsby API
   await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
@@ -105,5 +143,6 @@ export async function createPages(params) {
   await Promise.all([
     turnPizzasIntoPages(params),
     turnToppingsIntoPages(params),
+    turnSliceMastersIntoPages(params),
   ]);
 }
